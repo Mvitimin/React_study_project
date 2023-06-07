4582e4e1c98666471b1ccb2eb1ddd3b1041390e0
myoungwon <myoungwon.hur@navercorp.com>
2023-06-07 22:54:16 +0900

    State System in React

--- src/index.js
+++ src/index.js
@@ -2,21 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 class App extends React.Component {
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                console.log(position);
-            },
-            (error) => {
-                console.log(error);
-            }
-        );
-        return (
-            <div>
-                You are in the northern hemisphere
-            </div>
-        )
+
+  constructor(props) {
+    super(props)
+
+    this.state = { latitude: null, errorMessage: '' }
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ latitude: position.coords.latitude });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
+    );
+
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.latitude) {
+      return <div> {this.state.errorMessage} </div>
     }
+
+    if (!this.state.errorMessage && this.state.latitude) {
+      return <div> {this.state.latitude} </div>
+    }
+
+    else {
+      return <div>Loading...</div>
+    }
+
+  }
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
